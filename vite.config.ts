9a208b8d762e39bb36df3f0faf4533bcb0a89dde
Jawfree <jeoffreyb88@gmail.com>
2025-05-11 00:35:30 +0000

    Update project template
    Update project template to the latest Lovable version.

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import { componentTagger } from "lovable-tagger"
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
+    host: "::",
     port: 8080,
     proxy: {
       '/api': 'http://localhost:5000',
       '/v1_1': 'http://localhost:5000'
     }
   }
-})
+}))
