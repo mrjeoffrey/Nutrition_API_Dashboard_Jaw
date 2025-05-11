52706e580cdd597ed27fb338aecae92ca4a70689
Jawfree <jeoffreyb88@gmail.com>
2025-05-11 11:24:58 -0700

    Fix: Make Code Clean

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import { componentTagger } from "lovable-tagger"
+
 
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'development',
   ].filter(Boolean),
   resolve: {
     alias: {
