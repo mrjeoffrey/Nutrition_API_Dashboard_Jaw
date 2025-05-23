a09e5642caf57549943e0044134ffbcd26fbc6cc
Jawfree <jeoffreyb88@gmail.com>
2025-05-23 10:22:50 -0700

    Fix: CORS allow all

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: '*' }));;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
